d07c8a6ddd9cc1d93933e01aea6dc32b919944e9
Jun Zheng <arthurjordan@gmail.com>
2015-06-06 22:11:57 +1000

    second commit, argv from sys is used

---
+++ parameters_unpacking_argv.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+codee, first, second, third = argv
+
+print "The script is called: ", codee
+print "Your first variable is: ", first
+print "Your second variable is: ", second
+print "Your third variable is: ", third
---
+++ raw_input_exercise.py
@@ -0,0 +1,5 @@
+age = int(raw_input("Your age: "))
+name = raw_input("Your name: ")
+weight = raw_input("Your weight: ")
+
+print "So, your name is %s, and your age is %d, and your weight is %r  in raw format (so it's a string, not an integer)" %(name, age, weight)
